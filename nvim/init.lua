a540ffdac3795846ddebb09b7bcce3d645294ee0
deffucs <24848310+deffucs@users.noreply.github.com>
2026-04-10 12:23:17 -0400

    Update init.lua

--- nvim/init.lua
+++ nvim/init.lua
@@ -14,69 +14,92 @@ vim.o.undofile = true
 vim.o.incsearch = true
 vim.o.shiftwidth = 2
 vim.o.completeopt = "fuzzy,menuone,noinsert,nosort,popup"
+vim.o.foldenable = false
 
 vim.g.go_code_completion_enabled = 0
 vim.g.go_code_completion_enabled = 0
 
+-- Global Keymaps
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>v', ':e $MYVIMRC<CR>', { desc = 'Edit Vim config file' })
+vim.keymap.set('n', '<leader>o', ':update<CR>:source $MYVIMRC<CR>', { desc = 'Upadte and Source Vim config file' })
 
-vim.keymap.set('n', '<leader>o', ':update<CR>:source<CR>')
--- vim.keymap.set('n', '<leader>w', ':write<CR>')
-vim.keymap.set('n', '<leader>w', ':wa<CR>')
-vim.keymap.set('n', '<leader>q', ':quit<CR>')
+vim.keymap.set('n', '<leader>w', ':wa<CR>', { desc = 'Write all buffers' })
+vim.keymap.set('n', '<leader>q', ':quit<CR>', { desc = 'Quit Vim' })
 
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y<CR>')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d<CR>')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>v', ':e $MYVIMRC<CR>')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>s', ':e #<CR>')
-vim.keymap.set({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>d', '"+d', { desc = 'Delete to system clipboard' })
 
-vim.keymap.set({ 'n', 'v', 'x' }, '<C-j>', '<C-w>j')
-vim.keymap.set({ 'n', 'v', 'x' }, '<C-k>', '<C-w>k')
-vim.keymap.set({ 'n', 'v', 'x' }, '<C-h>', '<C-w>h')
-vim.keymap.set({ 'n', 'v', 'x' }, '<C-l>', '<C-w>l')
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>s', ':e #<CR>', { desc = 'Toggle to previous file' })
+vim.keymap.set({ 'n', 'v', 'x' }, '<leader>S', ':sf #<CR>', { desc = 'Open previous file in horizontal split' })
 
-vim.keymap.set('i', '<C-j>', '<C-n>')
-vim.keymap.set('i', '<C-k>', '<C-p>')
-vim.keymap.set('i', '<CR>', "pumvisible()? '<C-y>' : '<CR>'", { noremap = true, silent = true, expr = true })
+vim.keymap.set({ 'n', 'v', 'x' }, '<C-j>', '<C-w>j', { desc = 'Navigate window down' })
+vim.keymap.set({ 'n', 'v', 'x' }, '<C-k>', '<C-w>k', { desc = 'Navigate window up' })
+vim.keymap.set({ 'n', 'v', 'x' }, '<C-h>', '<C-w>h', { desc = 'Navigate window left' })
+vim.keymap.set({ 'n', 'v', 'x' }, '<C-l>', '<C-w>l', { desc = 'Navigate window right' })
+
+vim.keymap.set('i', '<C-j>', '<C-n>', { desc = 'Select next item in completion window' })
+vim.keymap.set('i', '<C-k>', '<C-p>', { desc = 'Select previous item in completion window' })
+vim.keymap.set('i', '<CR>', "pumvisible()? '<C-y>' : '<CR>'",
+	{ noremap = true, silent = true, expr = true, desc = 'Accept current selection of a popup menu' })
 
 vim.pack.add({
-	-- colorscheme
-	{ src = "https://github.com/morhetz/gruvbox.git" },
-	-- oil - file explorer
-	{ src = "https://github.com/stevearc/oil.nvim" },
-	-- mini.pick - fuzzy finder
-	{ src = "https://github.com/echasnovski/mini.pick" },
-	-- collection of language server configurations
-	{ src = "https://github.com/neovim/nvim-lspconfig" },
 	{
+		descritpion = 'Colorscheme',
+		src = "https://github.com/morhetz/gruvbox.git"
+	},
+	-- oil - file explorer
+	{
+		descritpion = 'File Explorer',
+		src = "https://github.com/stevearc/oil.nvim"
+	},
+	{
+		descritpion = 'Fuzzy Finder',
+		src = "https://github.com/echasnovski/mini.pick"
+	},
+	{
+		descritpion = 'Collection of language server configurations',
+		src = "https://github.com/neovim/nvim-lspconfig"
+	},
+	{
+		descritpion = 'Completion Plugin',
 		src = "https://github.com/saghen/blink.cmp",
 		version = 'v1.7.0',
-		opts = {
-			keymap = {
-				preset = 'enter',
-			},
-			completion = { documentation = { auto_show = true } },
-		},
 	},
-
 	-- Mason would be for installing LSPs
-	-- nvim-treesitter for fixing niche highlighting issues in some cases
+	{
+
+		descritpion = 'Formatting / Highlighting / Language Structure Helper',
+		src = "https://github.com/nvim-treesitter/nvim-treesitter"
+	},
+	{
+
+		descritpion = 'REST Client',
+		src = "https://github.com/mistweaverco/kulala.nvim",
+
+	},
+	--{ src = "https://github.com/nvim-neotest/nvim-nio" },
+
+	--{ src = "https://github.com/j-hui/fidget.nvim" },
+	-- { src = "https://github.com/rest-nvim/tree-sitter-http" },
 })
 
 -- Uncomment if a package update is needed
 -- vim.pack.del({ 'blink.cmp' })
 -- vim.pack.update({ 'blink.cmp' })
 
+-- Automatically connect LSP to buffers that support it
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(ev)
 		local client = vim.lsp.get_client_by_id(ev.data.client_id)
-		if client:supports_method('textDocument/completion') then
-			vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+		if client then
+			if client:supports_method('textDocument/completion') then
+				vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+			end
 		end
 	end,
 })
+-- If no completion menu option is manually selected, then accepting will not pick anything
 vim.cmd("set completeopt+=noselect")
-
 
 -- Autoformat on save
 --vim.api.nvim_create_autocmd("BufWritePre", {
@@ -112,7 +135,35 @@ vim.api.nvim_create_autocmd("BufWritePre", {
 
 require "mini.pick".setup()
 require "oil".setup()
+require "nvim-treesitter.configs".setup({
+	highlight = {
+		enable = true
+	}
+})
+require("kulala.config").setup({
+	keys = {
+		{ "<leader>Rs", desc = "Send request" },
+		{ "<leader>Ra", desc = "Send all requests" },
+		{ "<leader>Rb", desc = "Open scratchpad" },
+	},
+	ft = { "http", "rest" },
+	opts = {
+		global_keymaps = true,
+		global_keymaps_prefix = "<leader>R",
+		kulala_keymaps_prefix = "",
+	},
+})
 
+require("blink.cmp").setup({
+	keymap = {
+		preset = 'enter',
+		['<C-k>'] = { 'select_prev', 'fallback' },
+		['<C-j>'] = { 'select_next', 'fallback' },
+	},
+	completion = { documentation = { auto_show = true } },
+})
+
+-- Fuzzy Finder and File Nav Keymaps
 vim.keymap.set('n', '<leader>ff', ':Pick files<CR>')
 vim.keymap.set('n', '<leader>fg', ':Pick grep_live<CR>')
 vim.keymap.set('n', '<leader>h', ':Pick help<CR>')
